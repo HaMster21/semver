5d2be334bf701c8a3c7af9de9bcd2c2102267da0
Hans <hamster.of.dev@gmail.com>
2014-06-23 21:32:21 +0200

    Refactoring the regular expression
    The old one had the problem that in prerelease and build strings combinations like `+.---...` or `--...-..--` were valid. This version also invalidates versions starting with `0.0.0`.
    
    See http://goo.gl/8mg75m for visual details

--- source/semver.d
+++ source/semver.d
@@ -1,6 +1,7 @@
-import std.traits, std.regex, std.string;
+import std.traits, std.regex;
 
-enum semVerPattern = r"(?P<major>[0]|[1-9][0-9]*?)\.(?P<minor>[0]|[1-9][0-9]*?)\.(?P<patch>[0]|[1-9][0-9]*)(?P<prerelease>-[a-zA-Z0-9-.]+)?(?P<build>\+[a-zA-Z0-9-.]+)?"; 
+enum semVerPattern = 
+    r"(?P<major>0|[1-9][0-9]*?)\.(?P<minor>0|[1-9][0-9]*?)\.(?P<patch>0|[1-9][0-9]*)(?P<prerelease>-[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*)?(?P<build>\+[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*)?"; 
 
 struct SemVer {
     uint major;
@@ -13,26 +14,46 @@ struct SemVer {
 bool isValidSemver(Version) (Version versionString)
   if (isSomeString!Version)
 {
-    return match(versionString, semVerPattern).captures[0] == versionString;
+    import std.algorithm: startsWith;
+    return   !versionString.startsWith("0.0.0") 
+           && match(versionString, semVerPattern).captures[0] == versionString;
 }
 
 unittest {
+    assert(!isValidSemver("0.0.0-alpha.12-15+build.today-14pm")); 
+
     assert(!isValidSemver("1"));
     assert(!isValidSemver("1.0"));
     assert(!isValidSemver("9600.16384.130821.1623"));
-    assert(!isValidSemver("a.b.c"));
 
-    assert( isValidSemver("0.1.0"));
-    assert( isValidSemver("1.123.4"));
-
+    assert(!isValidSemver("01.0.0"));
+    assert(!isValidSemver("0.09.18"));
+    assert(!isValidSemver("14.0.007"));
+    
     assert(!isValidSemver("-4.0.12"));
     assert(!isValidSemver("0.-14.3"));
     assert(!isValidSemver("8.42.-17"));
 
-    assert( isValidSemver("172.13242.987034-beta.eta-kappa-123.41+csum-sha1.73409ABCDEF"));
+    assert(!isValidSemver("a.b.c"));
+    assert(!isValidSemver("a.0.0"));
+    assert(!isValidSemver("1.b.0"));
+    assert(!isValidSemver("2.1.c"));
 
-    assert(!isValidSemver("01239+-"));
+    assert(!isValidSemver("0.12.39+-"));
+    assert(!isValidSemver("1.0.0-"));
+    assert(!isValidSemver("1.0.0+"));
+    assert(!isValidSemver("1.1.2-4+"));
+    assert(!isValidSemver("4.1.7+14-"));
+
+    assert(!isValidSemver("0.0.1+a+b"));
+    
+    assert(!isValidSemver("1.0.0+軟件.12-1"));
+    assert(!isValidSemver("1.0.0-программное обеспечение"));
+
+    assert( isValidSemver("0.1.0"));
+    assert( isValidSemver("1.123.4"));
     assert( isValidSemver("1.2.3-prerelease"));
+    assert( isValidSemver("172.13242.987034-beta.eta-kappa-123.41+csum-sha1.73409ABCDEF"));
 
 }
 
