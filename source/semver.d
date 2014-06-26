ac490c5315caa0c4c88b87ce951245e1de3d27ef
Hans <hamster.of.dev@gmail.com>
2014-06-26 21:56:03 +0200

    Introducing more string encodings
    The matching function is now able to match char, wchar and dchar strings :smile:

--- source/semver.d
+++ source/semver.d
@@ -1,7 +1,10 @@
 import std.traits, std.regex;
 
-enum semVerPattern = 
-    r"(?P<major>0|[1-9][0-9]*?)\.(?P<minor>0|[1-9][0-9]*?)\.(?P<patch>0|[1-9][0-9]*)(?P<prerelease>-[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*)?(?P<build>\+[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*)?"; 
+template Pattern(StringType) {
+   import std.conv;
+   auto rx = regex(to!StringType(`(?P<major>0|[1-9][0-9]*?)\.(?P<minor>0|[1-9][0-9]*?)\.(?P<patch>0|[1-9][0-9]*)(?P<prerelease>-[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*)?(?P<build>\+[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*)?`));
+   alias Pattern = rx;
+}
 
 struct SemVer {
     uint major;
@@ -16,7 +19,7 @@ bool isValidSemver(Version) (Version versionString)
 {
     import std.algorithm: startsWith;
     return   !versionString.startsWith("0.0.0") 
-           && match(versionString, semVerPattern).captures[0] == versionString;
+           && match(versionString, Pattern!Version).captures[0] == versionString;
 }
 
 unittest {
@@ -55,10 +58,8 @@ unittest {
     assert( isValidSemver("1.2.3-prerelease"));
     assert( isValidSemver("172.13242.987034-beta.eta-kappa-123.41+csum-sha1.73409ABCDEF"));
 
-// TODO: This does not yet compile due to type mismatch in the template instantiantion of match
-//    assert( isValidSemver("1.0.0-alpha.1-4+sha1.afe12a98b"d));
-//    assert( isValidSemver("1.0.0-alpha.1-4+sha1.afe12a98b"w));
-
+    assert( isValidSemver("1.0.0-alpha.1-4+sha1.afe12a98b"d));
+    assert( isValidSemver("1.0.0-alpha.1-4+sha1.afe12a98b"w));
 }
 
 // TODO: compare two versions
