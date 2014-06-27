603fa789a3be3b25484400b2b42ac1f7458c24f2
Hans <hamster.of.dev@gmail.com>
2014-06-27 23:22:57 +0200

    Version validation on the command line
    dub will now build the semver application with the first command line usage.
    
    Given the -v or --validate option and one version string, the application will return 1 if the version string is not compliant to the Semantic Versioning specification. If the version string is okay, 0 is returned.
    
    In order to test the output of the command line tool and to keep cli interface in sync with the future library interface, the test folder has been created. It contains a test script for testing the command line tool and the expectations of the version validation.

--- source/app.d
+++ source/app.d
@@ -2,7 +2,39 @@
 
 import std.stdio;
 
-void main()
+int main(string[] args)
 {
-	writeln("SemVer Version 0.1.0.");
+   debug std.stdio.writefln("Args: %s", args);
+
+   if (args.length == 1) {
+       printAbout();
+       return 0;
+   }
+
+   switch (args[1]) {
+       default: std.stdio.writeln("Option not supported"); break;
+       case "-v" ,"--validate": {
+           if (args.length > 2) {
+               import semver: isValidSemver;
+               if (isValidSemver(args[2])) return 0; else return 1;
+           }
+           else {
+               std.stdio.writeln("a version string is needed for evaluation. Try 'semver -v 1.2.3'");
+               break;
+           }
+       }
+   }
+   return 0;
+}
+
+void printAbout() {
+    import std.stdio: writefln;
+
+    writefln("");
+    writefln("  semver Version 0.1.0");
+    writefln("  https://github.com/mog-wi/semver");
+    writefln("");
+    writefln("  Usage:");
+    writefln("    -v"); 
+    writefln("    --validate <version> Validates a version string. Returns 0 on success, 1 when passed an invalid semantic version");
 }
